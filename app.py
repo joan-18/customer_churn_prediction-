d40a978edadaea9fb9270b9688d83feb57692deb
joan-18 <sanyaolujoan@gmail.com>
2026-05-26 12:37:01 +0100

    update

--- app.py
+++ app.py
@@ -7,12 +7,15 @@ app = Flask(__name__)
 CORS(app)
 
 
-with open("models/preprocessor.pkl", "rb") as f:
+import os
+
+base_dir = os.path.dirname(__file__)
+
+with open(os.path.join(base_dir, "models/preprocessor.pkl"), "rb") as f:
     preprocessor = pickle.load(f)
 
-with open("models/classifier.pkl", "rb") as f:
+with open(os.path.join(base_dir, "models/classifier.pkl"), "rb") as f:
     classifier = pickle.load(f)
-
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
