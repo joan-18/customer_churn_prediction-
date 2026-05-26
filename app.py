c619e8ed11ab5fb3f519a13db2a807e723d3b1e0
joan-18 <sanyaolujoan@gmail.com>
2026-05-26 10:41:06 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import pickle
+import pandas as pd
+
+app = Flask(__name__)
+CORS(app)
+
+# Load the two separate pickle files
+with open("models/preprocessor.pkl", "rb") as f:
+    preprocessor = pickle.load(f)
+
+with open("models/classifier.pkl", "rb") as f:
+    classifier = pickle.load(f)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+
+    row = pd.DataFrame([{
+        "Gender":           data["gender"],
+        "Seniorcitizen":    int(data["seniorCitizen"]),
+        "Partner":          data["partner"],
+        "Dependents":       data["dependents"],
+        "Tenure":           float(data["tenure"]),
+        "Phoneservice":     data["phoneService"],
+        "Multiplelines":    data["multipleLines"],
+        "Internetservice":  data["internetService"],
+        "Onlinesecurity":   data["onlineSecurity"],
+        "Onlinebackup":     data["onlineBackup"],
+        "Deviceprotection": data["deviceProtection"],
+        "Techsupport":      data["techSupport"],
+        "Streamingtv":      data["streamingTV"],
+        "Streamingmovies":  data["streamingMovies"],
+        "Contract":         data["contract"],
+        "Paperlessbilling": data["paperlessBilling"],
+        "Paymentmethod":    data["paymentMethod"],
+        "Monthlycharges":   float(data["monthlyCharges"]),
+        "Totalcharges":     float(data["tenure"]) * float(data["monthlyCharges"]),
+    }])
+
+    X = preprocessor.transform(row)
+    prediction  = classifier.predict(X)[0]
+    probability = classifier.predict_proba(X)[0]
+
+    return jsonify({
+        "churn":       bool(prediction),
+        "probability": round(float(probability[1]) * 100, 1),
+        "accuracy":    82.0
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
